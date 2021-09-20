cfe630394a84ee8baa71a2be9ad0ae48f275eb3d
Trevor <56496067+Hifumi-Sec@users.noreply.github.com>
2021-09-20 13:19:21 -0500

    v1.0.1

--- config/__version__.py
+++ config/__version__.py
@@ -1,7 +1,7 @@
 #################################################################
-# Waifu Provider | v1.0.0                                       #
+# Waifu Provider | v1.0.1                                       #
 # Creator: Hifumi Sec (https://github.com/Hifumi-Sec)           #
 # Open-Source: https://github.com/Melancholy-Labs/waifuprovider #
 #################################################################
 
-VERSION = 'v1.0.0'
+VERSION = 'v1.0.1'
